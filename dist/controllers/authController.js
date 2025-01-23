2b69eaf72f2a6d28c4ca7a216d9e8289d3ed5646
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-01-23 15:54:31 +0530

    added scripts to package.json

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -7,8 +7,7 @@ const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 export const authController = {
     google: async (req, res) => {
-        //step 1, simply redirect to google oauth url
-        // const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+        //go to consent screen
         const url = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&scope=openid%20email%20profile%20&response_type=code&state=login`;
         return res.status(302).redirect(url);
     },
@@ -17,7 +16,6 @@ export const authController = {
         if (!GOOGLE_ACCESS_TOKEN_URL) {
             return res.status(400).json({ message: "google access token url not found" });
         }
-        // step 2, get AUTHORIZATION_CODE from query
         const { code } = req.query;
         console.log(code);
         if (!code) {
@@ -30,8 +28,7 @@ export const authController = {
             redirect_uri: GOOGLE_CALLBACK_URL,
             grant_type: "authorization_code",
         };
-        console.log(data);
-        //step 3, exchange AUTHORIZATION_CODE for ACCESS_TOKEN
+        //exchange AUTHORIZATION_CODE for ACCESS_TOKEN
         const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
             method: "POST",
             headers: {
@@ -41,17 +38,14 @@ export const authController = {
         });
         const accessTokenData = await response.json();
         const { id_token } = accessTokenData;
-        console.log(id_token);
-        // step 4, verify the id_token (manual is risky, so we use google-auth-library)
+        // verify id_token (manual is risky, so i use google-auth-library)
         const ticket = await client.verifyIdToken({
             idToken: id_token,
             audience: GOOGLE_CLIENT_ID,
         });
-        // step 5, get info from the ticket
         const payload = ticket.getPayload();
         if (!payload)
             return res.status(400).json({ message: "invalid token payload" });
-        console.log(payload);
         return res.redirect("http://localhost:5173");
     }
 };
