395a73cc690e2de7f212e6d580b7b4bc04403111
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-01-28 16:43:38 +0530

    deployed

--- src/utils/sendMagicLink.ts
+++ src/utils/sendMagicLink.ts
@@ -4,15 +4,27 @@ const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 const resend = new Resend(RESEND_API_KEY);
 
-export default async function sendMagicLink(email: string, token: string){
-    if(!RESEND_API_KEY) throw new Error("RESEND_API_KEY is not defined");
-    if(!email || !token) throw new Error("email or token is not defined");
+export default async function sendMagicLink(email: string, token: string) {
+    if (!RESEND_API_KEY) throw new Error("RESEND_API_KEY is not defined");
+    if (!email || !token) throw new Error("email or token is not defined");
     await resend.emails.send({
-        from: "Acme <onboarding@resend.dev>",
+        from: "Tabish <onboarding@usecrimson.me>",
         to: [email],
-        subject: "Login to your account",
+        subject: "Sign in to your account",
         html: `
-            <a href="http://localhost:8080/auth/verify?token=${token}">Click here to login</a>
-        `
+  <body>
+      <p>Hi there, <br /><br />
+    We noticed a login attempt to your Crimson account. To continue, please click the button below to verify your identity and log in securely:</p>
+        <a href="http://localhost:8080/auth/verify?token=${token}">
+        Click here to login
+    </a>
+    <h3>Weren't expecting this email?</h3>
+<p>If you weren't trying to sign in to Crimson, you can safely ignore this email. Someone must have incorrectly entered your email address.</p>
+<h3 >Need Help?</h3>
+  <p>
+    If you're having trouble logging in or didn't request this email, feel free to reach out to our support team at tabish.naqvi2003@gmail.com
+  </p>
+  </body>
+</html>`
     })
 }
